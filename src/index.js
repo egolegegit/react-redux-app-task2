dbaaf3cb05c325c2dcc4046fff480323b54ad8f0
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-01-01 23:06:33 +0700

    8. Чистые функции (pure functions)

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { compose, pipe } from 'lodash/fp'
 
 const App = (params) => {
-  const x = 2
+ 
 
-  const double = (number) => number * 2
-  const square = (number) => number * number
-  const half = (number) => number / 2
-
-  const divide = (num2) => {
-    return function (num1) {
-      return num1 / num2
-    }
-  }
-
-  // const mathCalculate = compose(half, square, double)
-  const mathCalculate = pipe(double, square, half, divide(3))
-
-  return <h1>{mathCalculate(x)}</h1>
+  return <h1>Apps</h1>
 }
 
 ReactDOM.render(
