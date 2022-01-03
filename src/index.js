54a310eef541d2b86bd25a1200b39d12a82d3fa4
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-01-03 16:37:30 +0700

    10,11,12 Create Store / Dispatch / Reducer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (params) => {
- 
+function taskReducer(state, action) {
+  switch (action.type) {
+    case 'task/complited':
+      const newArray = [...state]
+      const elementIndex = newArray.findIndex(
+        (item) => item.id === action.payload.id
+      )
+      newArray[elementIndex].complited = true
+      return newArray
 
-  return <h1>Apps</h1>
+    default:
+      break
+  }
+}
+
+function createStore(reducer, initialState) {
+  let state = initialState
+
+  function getState() {
+    return state
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action)
+  }
+
+  return { getState, dispatch }
+}
+
+const store = createStore(taskReducer, [
+  { id: 1, description: 'Task 1', complited: false },
+])
+
+const App = () => {
+  const completeTask = () => {
+    store.dispatch({ type: 'task/complited', payload: { id: 1 } })
+    console.log('store.getState()', store.getState())
+  }
+  console.log('store.getState()', store.getState())
+
+  return (
+    <>
+      <h1>Apps</h1>
+      <button onClick={completeTask}>Complete</button>
+    </>
+  )
 }
 
 ReactDOM.render(
