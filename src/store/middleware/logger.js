4aafe9f914644f9bbddab64b6b5ef10b022d2463
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-01-06 19:32:44 +0700

    28. Создаём middleware. Логгер 2

--- src/store/middleware/logger.js
+++ src/store/middleware/logger.js
@@ -1,13 +1,6 @@
-export function logger({ getState, dispatch }) {
+export function logger(store) {
   return function wrapDispatch(next) {
     return function handleAction(action) {
-      console.log('next', next)
-      console.log('action', action)
-
-      if (action.type === 'task/update') {
-        return dispatch({ type: 'task/remove', payload: { ...action.payload } })
-      }
-
       return next(action)
     }
   }
